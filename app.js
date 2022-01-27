fafe6905e96cfcf3b4be8d4943a40edc490e8d14
smarsha92 <70488663+smarsha92@users.noreply.github.com>
2022-01-27 12:49:35 -0500

    h

--- app.js
+++ app.js
@@ -64,3 +64,34 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
+//Title preview //
+const getTitle = async page => {
+    const title = await page.evaluate(() => {
+        const ogTitle = document.querySelector('meta[property="og:title"]');
+        if (ogTitle != null && ogTitle.content.length > 0) {
+            return ogTitle.content;
+        }
+        const twitterTitle = document.querySelector('meta[name="twitter:title"]');
+        if (twitterTitle != null && twitterTitle.content.length > 0) {
+            return twitterTitle.content;
+        }
+        const docTitle = document.title;
+        if (docTitle != null && docTitle.length > 0) {
+            return docTitle;
+        }
+        const h1 = document.querySelector("h1").innerHTML;
+        if (h1 != null && h1.length > 0) {
+            return h1;
+        }
+        const h2 = document.querySelector("h1").innerHTML;
+        if (h2 != null && h2.length > 0) {
+            return h2;
+        }
+        return null;
+    });
+    return title;
+};
+//end//
+
+//Description//
